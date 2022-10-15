55329f92c2e60cbfb6850d1c32a28cd52d849bb3
Ivan Tabarino <tabarino@outlook.com>
2022-10-15 15:23:32 +0100

    chore: install eslint and prettier on the project

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unstable-nested-components': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'no-alert': 'off',
+  },
+};
