86b2b3765fee4d98b42f4c10cc60d4f128779638
Ivan Tabarino <tabarino@outlook.com>
2022-10-15 16:36:22 +0100

    feat: create a ListScreen and add a FlatList on it

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text } from 'react-native';
+
+function ListScreen(props) {
+  const friends = [
+    { name: 'Friend #1', age: 20 },
+    { name: 'Friend #2', age: 21 },
+    { name: 'Friend #3', age: 22 },
+    { name: 'Friend #4', age: 23 },
+    { name: 'Friend #5', age: 24 },
+    { name: 'Friend #6', age: 25 },
+    { name: 'Friend #7', age: 26 },
+  ];
+
+  return (
+    <FlatList
+      // horizontal
+      // showsHorizontalScrollIndicator={false}
+      data={friends}
+      keyExtractor={(friend) => friend.name}
+      renderItem={({ item }) => <Text style={styles.text}>{`${item.name} - Age: ${item.age}`}</Text>}
+      showsVerticalScrollIndicator={false}
+    />
+  );
+}
+
+export default ListScreen;
+
+const styles = StyleSheet.create({
+  text: {
+    marginVertical: 50,
+  },
+});
