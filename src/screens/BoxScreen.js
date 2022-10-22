dcba87e6639b6772736fd8f2576c51fd059d068d
Ivan Tabarino <tabarino@outlook.com>
2022-10-22 09:58:25 +0100

    feat: use flexbox to position items on the screen

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,22 +3,42 @@ import { StyleSheet, Text, View } from 'react-native';
 function BoxScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Box Screen</Text>
+      <Text style={styles.textOne}>Text1</Text>
+      <Text style={styles.textTwo}>Text2</Text>
+      <Text style={styles.textThree}>Text3</Text>
     </View>
   );
 }
 
 export default BoxScreen;
 
+// Note: alignSelf on child overrides alignItems on parent
+
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
     borderWidth: 3,
     borderColor: 'black',
+    height: 200,
   },
-  text: {
+  textOne: {
+    flex: 4,
+    alignSelf: 'flex-start',
     borderWidth: 3,
     borderColor: 'red',
-    margin: 20,
-    padding: 20,
+  },
+  textTwo: {
+    flex: 4,
+    alignSelf: 'flex-end',
+    borderWidth: 3,
+    borderColor: 'red',
+  },
+  textThree: {
+    flex: 2,
+    alignSelf: 'stretch',
+    borderWidth: 3,
+    borderColor: 'red',
   },
 });
