3382b11857dffc2f1521cf19aa731c43dbb7d502
Ivan Tabarino <tabarino@outlook.com>
2022-10-22 11:20:37 +0100

    feat: use position on elements

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -6,6 +6,9 @@ function BoxScreen() {
       <Text style={styles.textOne}>Text1</Text>
       <Text style={styles.textTwo}>Text2</Text>
       <Text style={styles.textThree}>Text3</Text>
+      <Text style={styles.textFour}>Text4</Text>
+      <Text style={styles.textFive}>Text5</Text>
+      <Text style={styles.textSix}>Text6</Text>
     </View>
   );
 }
@@ -18,7 +21,7 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     borderWidth: 3,
     borderColor: 'black',
     height: 200,
@@ -38,6 +41,30 @@ const styles = StyleSheet.create({
   textThree: {
     flex: 2,
     alignSelf: 'stretch',
+    borderWidth: 3,
+    borderColor: 'red',
+  },
+  textFour: {
+    // position: 'absolute',
+    // top: 10,
+    bottom: 10,
+    left: 10,
+    borderWidth: 3,
+    borderColor: 'red',
+  },
+  textFive: {
+    right: 10,
+    borderWidth: 3,
+    borderColor: 'red',
+  },
+  textSix: {
+    // position: 'absolute',
+    // top: 0,
+    // bottom: 0,
+    // left: 0,
+    // right: 0,
+    // The same as above
+    ...StyleSheet.absoluteFill,
     borderWidth: 3,
     borderColor: 'red',
   },
